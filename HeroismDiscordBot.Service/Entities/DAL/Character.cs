717453cede2267ba0cd20537b3b291430188fdee
Sephiroth018 <Sephiroth018@users.noreply.github.com>
2018-03-18 19:45:45 +0100

    refactoring

--- HeroismDiscordBot.Service/Entities/DAL/Character.cs
+++ HeroismDiscordBot.Service/Entities/DAL/Character.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using JetBrains.Annotations;
